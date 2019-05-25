9acd14c47b477e4d029527470b884cf874badaae
Joshua <xiang2903@gmail.com>
2019-05-25 18:37:18 +0800

    added login and signup forms

--- customer-app/src/App.js
+++ customer-app/src/App.js
@@ -1,13 +1,56 @@
+import React from 'react';
+import './App.css';
+import Axios from 'axios';
+import FormContainer from './components/FormContainer';
+import { Route, Switch } from "react-router-dom";
 import React from "react";
 import "./App.css";
 import OrderForm from "./components/OrderForm.js";
 
-function App() {
-  return (
-    <>
-      <OrderForm />
-    </>
-  );
+
+
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      users: [],
+      currentuser: "",
+      isLoading: true,
+      message: ""
+    }
+  }
+
+  componentDidMount() {
+    Axios('localhost:5000/api/v1/users')
+      .then(data => {
+        this.setState({
+          users: data.data,
+          isLoading: false
+        })
+      }
+      )
+      .catch(error => {
+        console.log(error)
+      })
+  }
+
+  render() {
+    const { isLoading, users } = this.state
+    return (
+      <>
+        {/* <Navbar users={users} /> */}
+
+        <Switch>
+
+          <Route exact path="/" component={props => <FormContainer {...props} isLoading={isLoading} users={users} />} />
+
+
+        </Switch>
+
+
+      </>
+    )//end of return
+  }//end of render
 }
 
 export default App;
--- customer-app/src/index.js
+++ customer-app/src/index.js
@@ -6,8 +6,19 @@ import * as serviceWorker from "./serviceWorker";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "react-datepicker/dist/react-datepicker.css";
 import "react-dropzone-uploader/dist/styles.css";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter } from 'react-router-dom';
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById('root'));
+
+ReactDOM.render(
+    <BrowserRouter><App /></BrowserRouter>,
+    document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
