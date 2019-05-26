b20dc0aa58c6fdeb7df7ac9a68a2a6d5db3f775d
mei <yimei.wym@gmail.com>
2019-05-26 16:31:29 +0800

    fix imports

--- customer-app/src/index.js
+++ customer-app/src/index.js
@@ -1,23 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { BrowserRouter } from 'react-router-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
 import "react-datepicker/dist/react-datepicker.css";
 import "react-dropzone-uploader/dist/styles.css";
-import { BrowserRouter } from 'react-router-dom';
-
-
-ReactDOM.render(
-    <BrowserRouter><App /></BrowserRouter>,
-    document.getElementById('root'));
-
+import * as serviceWorker from "./serviceWorker";
+import App from "./App";
+import "./index.css";
 
 ReactDOM.render(
-    <BrowserRouter><App /></BrowserRouter>,
-    document.getElementById('root'));
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  document.getElementById("root")
+);
+
+ReactDOM.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  document.getElementById("root")
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
