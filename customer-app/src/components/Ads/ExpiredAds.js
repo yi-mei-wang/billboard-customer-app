b2f37561de926470143fe61185c7b8c212b18da5
mei <yimei.wym@gmail.com>
2019-05-29 19:01:44 +0800

    call endpoint for expired ads

--- customer-app/src/components/Ads/ExpiredAds.js
+++ customer-app/src/components/Ads/ExpiredAds.js
@@ -1,9 +1,28 @@
 import React from "react";
+import axios from "axios";
+
+const server = "localhost:5000";
 
 class ExpiredAds extends React.Component {
-    render() {
+  state = {
+    orders: []
+  };
 
-        return <h1>Expired ADs</h1>
-    }
+  componentDidMount() {
+    let token = localStorage.getItem("jwt");
+    axios
+      .get(`http://${server}/api/v1/orders/show?q=-1`, {
+        headers: { Authorization: `Bearer ${token}` }
+      })
+      .then(results => {
+        console.log(results);
+      })
+      .catch(errors => {
+        console.log(errors);
+      });
+  }
+  render() {
+    return <h1>Expired ADs</h1>;
+  }
 }
 export default ExpiredAds;
