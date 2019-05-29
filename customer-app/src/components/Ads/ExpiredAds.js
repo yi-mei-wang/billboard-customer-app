bf07661c2c1004821bf36913747cf407bb4013fb
mei <yimei.wym@gmail.com>
2019-05-29 20:49:53 +0800

    display expired ads

--- customer-app/src/components/Ads/ExpiredAds.js
+++ customer-app/src/components/Ads/ExpiredAds.js
@@ -14,15 +14,48 @@ class ExpiredAds extends React.Component {
       .get(`http://${server}/api/v1/orders/show?q=-1`, {
         headers: { Authorization: `Bearer ${token}` }
       })
-      .then(results => {
-        console.log(results);
+      .then(response => {
+        console.log(response.data);
+        this.setState({
+          orders: response.data
+        });
       })
       .catch(errors => {
         console.log(errors);
       });
   }
+
   render() {
-    return <h1>Expired ADs</h1>;
+    const orders = this.state.orders.map(order => (
+      <>
+        <p>{order.order_id}</p>
+        <p>{order.start_time}</p>
+        {order.images.map(img => (
+          <img src={img} />
+        ))}
+      </>
+    ));
+    return (
+      <>
+        <div className={"px-4"}>
+          <h2 className={"pt-3 m-0"}>Expired ADs</h2>
+          <span
+            className={"underline mb-2"}
+            style={{
+              backgroundColor: "#ffbf00",
+              width: "200px",
+              height: "10px",
+              display: "inline-block"
+            }}
+          />
+          {orders}
+          {/* ----------- */}
+          {/* images: ["https://s3.amazonaws.com/meistagram/tech.jpeg"]
+order_id: 146
+start_time: "Wed, 29 May 2019 12:15:00 GMT" */}
+        </div>
+      </>
+    );
   }
 }
 export default ExpiredAds;
--- customer-app/src/components/Ads/ScheduledAds.js
+++ customer-app/src/components/Ads/ScheduledAds.js
@@ -1,9 +1,23 @@
 import React from "react";
 
 class ScheduledAds extends React.Component {
-    render() {
-
-        return <h1>Scheduled ADs</h1>
-    }
+  render() {
+    return (
+      <>
+        <div className={"px-4"}>
+          <h2 className={"pt-3 m-0"}>Scheduled ADs</h2>
+          <span
+            className={"underline mb-2"}
+            style={{
+              backgroundColor: "#ffbf00",
+              width: "200px",
+              height: "10px",
+              display: "inline-block"
+            }}
+          />
+        </div>
+      </>
+    );
+  }
 }
 export default ScheduledAds;
