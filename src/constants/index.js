96a950a88ab742b945ba8c37ccd08eec96d9d590
mei <yimei.wym@gmail.com>
2019-05-30 14:47:22 +0800

    change server url

--- src/constants/index.js
+++ src/constants/index.js
@@ -3,7 +3,7 @@ let DOMAIN_URL = ''
 if (process.env.NODE_ENV !== 'production') {
   DOMAIN_URL = 'http://localhost:5000'
 } else {
-  DOMAIN_URL = 'https://billboard-automator-server.herokuapp.com'
+  DOMAIN_URL = 'https://billboard-automated-server-1.herokuapp.com'
 }
 
 export { DOMAIN_URL }
