90d1c745f38ca0867d970ebd735df3cf80b4261b
mei <yimei.wym@gmail.com>
2019-09-13 14:00:04 +0800

    rename logo filename

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -1,6 +1,6 @@
 import React from "react";
 import styled from "styled-components";
-import logo from "./images/adlogo1.png";
+import logo from "./images/adlogo.png";
 
 const Button = styled.button`
   text-align: center;
